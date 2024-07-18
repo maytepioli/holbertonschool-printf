d158327802c07c5302b41a5da9c27a57abda1288
maytepioli <maytepioliggmail.com>
2024-07-18 16:21:43 +0000

    los casos ya funcionan, faltan los caos borde

---
+++ _printf.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ *
+ *
+ *
+ *
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i = 0;
+	char *str;
+	int contador;
+
+	va_start(ap, format);
+
+	while(format[i] != '\0')
+	
+	{
+		if (format[i] != '%')
+			_putchar(format[i]);
+		else 
+		{
+			i++;
+			switch(format[i])
+			{
+				case 'c':
+					
+					_putchar((char)va_arg(ap, int));
+					contador++;
+					break;
+
+				case 's':
+					str = va_arg(ap, char *);
+					if (str == NULL)
+					{
+						write(1,"(null)", 6);
+						contador += 6;
+					}
+					else
+					{
+						write(1, str, _strlen(str));
+						contador++;
+					}
+					break;
+
+				case '%':
+					_putchar('%');
+					contador++;
+					break;
+							
+			}
+
+
+		
+		}
+	i++;
+	}
+	return 0;
+}
