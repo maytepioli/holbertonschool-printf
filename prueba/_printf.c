939b145636b64e76280044c983105f9157e31063
maytepioli <maytepioli@gmail.com>
2024-07-21 15:21:10 +0000

    separe en funciones el codigo

---
+++ prueba/_printf.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+void print_string(char *str, int *contador);
+void print_default(const char *format, int *i, int *contador);
+
+/**
+ * print_string - Prints a string or "(null)" if the string is NULL.
+ * @str: The string to print.
+ * @contador: Pointer to the count of characters printed.
+ */
+void print_string(char *str, int *contador)
+{
+    int len;
+
+    if (str == NULL)
+    {
+        write(1, "(null)", 6);
+        *contador += 6;
+    }
+    else
+    {
+        len = _strlen(str);
+        write(1, str, len);
+        *contador += len;
+    }
+}
+/**
+ * print_default - Handles the default case for unsupported format specifiers.
+ * @format: The format string.
+ * @i: The current index in the format string.
+ * @contador: Pointer to the count of characters printed.
+ */
+void print_default(const char *format, int *i, int *contador)
+{
+    _putchar(format[*i]);
+    *contador += 1;
+    _putchar(format[*i + 1]);
+    *contador += 1;
+}
+
+/**
+ * _printf - Custom printf function.
+ * @format: The format string.
+ * @...: The values to format and print.
+ * Return: The number of characters printed, or -1 on failure.
+ */
+int _printf(const char *format, ...)
+{
+    va_list ap;
+    int i = 0;
+    char *str;
+    int contador = 0;
+
+    if (format == NULL)
+        return (-1);
+
+    va_start(ap, format);
+
+    while (format[i] != '\0')
+    {
+        if (format[i] != '%')
+        {
+            _putchar(format[i]);
+            contador++;
+        }
+        else
+        {
+            switch (format[i + 1])
+            {
+                case 'c':
+                    _putchar((char)va_arg(ap, int));
+                    contador++;
+                    i++;
+                    break;
+
+                case 's':
+                    str = va_arg(ap, char *);
+                    print_string(str, &contador);
+                    i++;
+                    break;
+}
+
+/**
+ * _printf - Custom printf function.
+ * @format: The format string.
+ * @...: The values to format and print.
+ * Return: The number of characters printed, or -1 on failure.
+ */
+int _printf(const char *format, ...)
+{
+    va_list ap;
+    int i = 0;
+    char *str;
+    int contador = 0;
+
+    if (format == NULL)
+        return (-1);
+
+    va_start(ap, format);
+
+    while (format[i] != '\0')
+    {
+        if (format[i] != '%')
+        {
+            _putchar(format[i]);
+            contador++;
+        }
+        else
+        {
+            switch (format[i + 1])
+            {
+                case 'c':
+                    _putchar((char)va_arg(ap, int));
+                    contador++;
+                    i++;
+                    break;
+
+                case 's':
+                    str = va_arg(ap, char *);
+                    print_string(str, &contador);
+                    i++;
+                case '%':
+                    _putchar('%');
+                    contador++;
+                    i++;
+                    break;
+
+                case '\0':
+                    contador = -1;
+                    break;
+
+                default:
+                    print_default(format, &i, &contador);
+                    break;
+            }
+        }
+        i++;
+    }
+
+    va_end(ap);
+    return (contador);
+}
---
+++ prueba/_strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - check the code
+ * @s: string
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+  int len;
+
+  for (len = 0; s[len] != '\0'; len++)
+  {
+  }
+  return (len);
+}
